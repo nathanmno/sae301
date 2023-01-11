4a937fcee696afde19e171fceb128a5ced8625ba
Diaks17 <71708204+Diaks17@users.noreply.github.com>
2023-01-11 22:58:25 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -16,3 +16,31 @@ function AfficheBtnEnglish() {
   document.querySelector(".english-version").style.display = "none";
   document.querySelector(".french-version").style.display = "block";
 }
+//Effet décompte des chiffres
+const counters = document.querySelectorAll(".chiffres");
+const speed = 300;
+function chiffresAnim() {
+  counters.forEach((counter) => {
+    const animate = () => {
+      const value = +counter.getAttribute("number");
+      const data = +counter.innerText;
+
+      const time = value / speed;
+      if (data < value) {
+        counter.innerText = Math.ceil(data + time);
+        setTimeout(animate, 100);
+      } else {
+        counter.innerText = value;
+      }
+    };
+    animate();
+  });
+}
+window.addEventListener(
+  "scroll",
+  () => {
+    if (window.scrollY >= 320) chiffresAnim();
+  },
+  true,
+  setTimeout(1000)
+);
